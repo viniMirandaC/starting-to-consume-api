daab0f47b1f2f5ce51ab0be6e33d5b5c53184fee
Vinícius Miranda <129119458+viniMirandaC@users.noreply.github.com>
2023-03-30 15:28:40 -0300

    Dart project starting to consume API

---
+++ acadFlutter/consumo_api_aluno/lib/models/endereco.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:consumo_api_aluno/models/cidade.dart';
+import 'package:consumo_api_aluno/models/telefone.dart';
+
+class Endereco {
+  String rua;
+  int numero;
+  String cep;
+  Cidade cidade;
+  Telefone telefone;
+  Endereco({
+    required this.rua,
+    required this.numero,
+    required this.cep,
+    required this.cidade,
+    required this.telefone,
+  });
+}
